481b0a37eec45d32e93478e9293f966e90b2585f
Hanson Yang(Mikuma hiroki) <101704292+Hanson0901@users.noreply.github.com>
2024-02-29 14:13:12 +0800

    Update main.c

--- EX1/main.c
+++ EX1/main.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
 
+void prt_space(int n,int m){
+    for(int i=0;i<m-n;i++){
+        printf(" ");
+    }
+}
+void Prt_num(int n) {
+    for(int i=0;i<n;i++){
+        printf("%d ",n);
+    }
+    printf("\n");
+}
+
+int main(){
+    int rowhigh =6;
+    for(int i=0;i<=rowhigh;i++){
+        prt_space(i,rowhigh);
+        Prt_num(i);
+    }
+    return 0;
+}
